e864d81307241d17c99bae8acbc223360c89145b
charbonnev <charles.bonnevialle@gmail.com>
2024-11-15 18:18:49 -0300

    cleanup and comment improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Simple Bot to reply to Telegram messages.
+Simple Bot to reply to Telegram voice messages with the text transcription.
 
 First, a few handler functions are defined. Then, those functions are passed to
 the Application and registered at their respective places.
@@ -7,18 +7,18 @@ Then, the bot is started and runs until we press Ctrl-C on the command line.
 
 Usage:
 Send the bot an audio, it will reply with the transcription.
+run it with python3 main.py
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
-import whisper
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-from telegram import ForceReply, Update
+from dotenv import load_dotenv
 import logging
 import os
-from dotenv import load_dotenv
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import whisper
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-
 
 # Load the Whisper model
 # Choose "tiny", "base", "small", "medium", or "large"
@@ -29,10 +29,9 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
+# if you want to see everything, put logging.INFO
 logging.getLogger("httpx").setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
-
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -40,23 +39,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+        rf"Hi {user.mention_html()} I'm an audio transcribing bot! Send me an audio and I'll reply with the transcription.",
         reply_markup=ForceReply(selective=True),
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
+    await update.message.reply_text("Send me and audio and I'll reply with the transcription.")
 
 async def echo_voice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Save the voice message to the current folder and Echo the user voice message."""
+    """Save the voice message to the current folder, transcribes it and sends it back."""
     voice_file = await update.message.voice.get_file()
     audio_path = f"{voice_file.file_id}.ogg"
     await voice_file.download_to_drive(audio_path)
@@ -73,10 +66,6 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(
-        filters.TEXT & ~filters.COMMAND, echo))
 
     # on receiving a voice message, send it back
     application.add_handler(MessageHandler(filters.VOICE, echo_voice))
